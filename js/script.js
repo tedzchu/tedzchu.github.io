f40526229ddadb1b7227912e99a95241fd4f7584
it-door <tedchu4@gmail.com>
2020-11-19 00:18:30 -0800

    JS formatting and end note

--- js/script.js
+++ js/script.js
@@ -1,60 +1,60 @@
 //reusable text rotation
 var TxtRotate = function (el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 1000;
-    this.txt = "";
-    this.tick();
-    this.isDeleting = false;
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 1000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
 };
 TxtRotate.prototype.tick = function () {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-    var that = this;
-    var delta = 230 - Math.random() * 100;
-    if (this.isDeleting) {
-        delta /= 2;
-    }
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-    setTimeout(function () {
-        that.tick();
-    }, delta);
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+  var that = this;
+  var delta = 230 - Math.random() * 100;
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+  setTimeout(function () {
+    that.tick();
+  }, delta);
 };
 window.onload = function () {
-    var elements = document.getElementsByClassName("txt-rotate");
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute("data-rotate");
-        var period = elements[i].getAttribute("data-period");
-        if (toRotate) {
-            new TxtRotate(elements[i], JSON.parse(toRotate), period);
-        }
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML =
-        ".txt-rotate > .wrap { border-right: 0.06em solid #3C288D }";
-    document.body.appendChild(css);
+  }
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  css.innerHTML = '.txt-rotate > .wrap { border-right: 0.06em solid #3C288D }';
+  document.body.appendChild(css);
 };
 
 //portfolio load script using jquery
 function modal(content) {
-    $("#loadMe").load("portfolio.html #" + content).then($(".modal").addClass("is-active"));
+  $('#loadMe').load('portfolio.html #' + content);
+  $('.modal').addClass('is-active');
 }
-$(".modal-close").click(function () {
-    $(".modal").removeClass("is-active");
-    $("#loadMe").html("");
+$('.modal-close').click(function () {
+  $('.modal').removeClass('is-active');
+  $('#loadMe').html('');
 });
